49f029daa0a1fdd560b2a7ea2099755cdaf1674e
Partha-56 <parthasarathinayak120@gmail.com>
2025-10-25 11:54:34 +0530

    new test case added

---
+++ src/test/java/com/actitime/testcases/TC_2.java
@@ -0,0 +1,11 @@
+package com.actitime.testcases;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TC_2 {
+	@Test
+	public void demo() {
+		Reporter.log("partha", true);
+	}
+}
